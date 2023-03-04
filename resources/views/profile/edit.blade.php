1809e96a21dc8e1d2e3ef809690bb0e8691b9912
micheleTummolo <tummolomichele02@gmail.com>
2023-03-04 02:19:18 +0100

    Done profile edit

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -2,9 +2,9 @@
 @section('content')
 
 <div class="container">
-    <h2 class="fs-4 text-secondary my-4">
+    <h1 class="fs-4 text-secondary my-4 text-white">
         {{ __('Profile') }}
-    </h2>
+    </h1>
     <div class="card p-4 mb-4 bg-white shadow rounded-lg">
 
         @include('profile.partials.update-profile-information-form')
